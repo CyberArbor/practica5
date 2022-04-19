1c3ca8cde06c6527f34d3b360bde33f85c4b5a95
CyberArbor <79295892+CyberArbor@users.noreply.github.com>
2022-04-18 19:00:03 -0700

    practica5

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Contact from './components/Contact';
+import Circulo from './components/Circulo';
+import Cuadro from './components/Cuadro';
+import Barra from './layout/Barra';
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter> 
+    <Routes>
+
+
+    <Route path='/' element={<Barra/>}>
+      <Route path='circulo' element={  <Circulo/>   }     />
+      <Route path='contacto' element={  <Contact/>   }     />
+      <Route path='cuadro' element={  <Cuadro/>   }     />
+
+    </Route>
+
+
+
+
+
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
